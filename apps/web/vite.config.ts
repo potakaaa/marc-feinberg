b5ca5a80fff1f06262f62fc87f72a08e5294a3ae
Rald Helbiro <helbirog@gmail.com>
2026-06-05 14:06:49 +0800

    chore(config): configured deployment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,11 @@ import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import netlify from "@netlify/vite-plugin-tanstack-start"
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
 })
 
 export default config
